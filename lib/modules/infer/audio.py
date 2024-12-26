68da753138577303d61c27a255d9ac2dc0f1275c
Player1444 <94734830+Player124413@users.noreply.github.com>
2024-12-26 11:52:57 +0300

    Add files via upload

--- lib/modules/infer/audio.py
+++ lib/modules/infer/audio.py
@@ -1,27 +1,15 @@
-import os
-import subprocess
-from io import BytesIO
-import traceback
-import sys
-import av
-import librosa
-import numpy as np
+import platform, os
 import ffmpeg
-
-platform_stft_mapping = {
-    "linux": "stftpitchshift",
-    "darwin": "stftpitchshift",
-    "win32": "stftpitchshift.exe",
-}
-
-stft = platform_stft_mapping.get(sys.platform)
+import numpy as np
+import av
+from io import BytesIO
 
 
-def wav2(input_file, output_file, format):
-    inp = av.open(input_file, "rb")
+def wav2(i, o, format):
+    inp = av.open(i, "rb")
     if format == "m4a":
         format = "mp4"
-    out = av.open(output_file, "wb", format=format)
+    out = av.open(o, "wb", format=format)
     if format == "ogg":
         format = "libvorbis"
     if format == "mp4":
@@ -40,89 +28,24 @@ def wav2(input_file, output_file, format):
     inp.close()
 
 
-def audio2(input_file, output_file, format, sr):
-    inp = av.open(input_file, "rb")
-    out = av.open(output_file, "wb", format=format)
-    if format == "ogg":
-        format = "libvorbis"
-    if format == "f32le":
-        format = "pcm_f32le"
-
-    ostream = out.add_stream(format, channels=1)
-    ostream.sample_rate = sr
-
-    for frame in inp.decode(audio=0):
-        for p in ostream.encode(frame):
-            out.mux(p)
-
-    out.close()
-    inp.close()
-
-
-def load_audio(file, sr, DoFormant=False, Quefrency=1.0, Timbre=1.0):
-    file = file.strip(' \n"')
-    if not os.path.exists(file):
-        raise RuntimeError(
-            "Wrong audio path, that does not exist."
+def load_audio(file, sr):
+    try:
+        # https://github.com/openai/whisper/blob/main/whisper/audio.py#L26
+        # This launches a subprocess to decode audio while down-mixing and resampling as necessary.
+        # Requires the ffmpeg CLI and `ffmpeg-python` package to be installed.
+        file = clean_path(file)  # 防止小白拷路径头尾带了空格和"和回车
+        out, _ = (
+            ffmpeg.input(file, threads=0)
+            .output("-", format="f32le", acodec="pcm_f32le", ac=1, ar=sr)
+            .run(cmd=["ffmpeg", "-nostdin"], capture_stdout=True, capture_stderr=True)
         )
+    except Exception as e:
+        raise RuntimeError(f"Failed to load audio: {e}")
 
-    converted = False
-    try:
-        if not file.endswith(".wav"):
-            converted = True
-            subprocess.run(
-                ["ffmpeg", "-nostdin", "-i", file, f"{file}.wav"],
-                capture_output=True,
-                text=True,
-            )
-            file = f"{file}.wav"
-            print(f"File formatted to wav format: {file}\n")
-
-        if DoFormant:
-            command = (
-                f'{stft} -i "{file}" -q "{Quefrency}" '
-                f'-t "{Timbre}" -o "{file}_formatted.wav"'
-            )
-            subprocess.run(command, shell=True)
-            file = f"{file}_FORMATTED.wav"
-            print(f"Formatted {file}\n")
-
-        with open(file, "rb") as f:
-            with BytesIO() as out:
-                audio2(f, out, "f32le", sr)
-                audio_data = np.frombuffer(out.getvalue(), np.float32).flatten()
-
-        if converted:
-            try:
-                os.remove(file)
-            except Exception as error:
-                print(f"Couldn't remove converted type of file due to {error}")
-                error = None
-            converted = False
-
-        return audio_data
-
-    except AttributeError:
-        audio = file[1] / 32768.0
-        if len(audio.shape) == 2:
-            audio = np.mean(audio, -1)
-        return librosa.resample(audio, orig_sr=file[0], target_sr=16000)
-    except Exception:
-        raise RuntimeError(traceback.format_exc())
+    return np.frombuffer(out, np.float32).flatten()
 
 
-def check_audio_duration(file):
-    try:
-        file = file.strip(' \n"')
-        probe = ffmpeg.probe(file)
-        duration = float(probe["streams"][0]["duration"])
-
-        if duration < 0.76:
-            print(
-                f"Audio file, {os.path.basename(file)}, under ~0.76s detected - file is too short. Target at least 1-2s for best results."
-            )
-            return False
-
-        return True
-    except Exception as error:
-        raise RuntimeError(f"Failed to check audio duration: {error}")
+def clean_path(path_str):
+    if platform.system() == "Windows":
+        path_str = path_str.replace("/", "\\")
+    return path_str.strip(" ").strip('"').strip("\n").strip('"').strip(" ")
